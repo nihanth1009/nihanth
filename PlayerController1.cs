b23f81f69e55b636ffb350ccc5ef6224986edbc2
nihanth1009 <105349379+nihanth1009@users.noreply.github.com>
2023-01-21 17:36:29 +0530

    Add files via upload

---
+++ PlayerController1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    
+    private void Update()
+    
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1,1);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1,1);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+
+    }
+
+
+
+
+
+}
